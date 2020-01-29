6b1acb55ba47d63c31afe6212f507a07360c1d56
Isaacraja830 <isaacrajamech92@gmail.com>
2020-01-29 10:46:25 +0530

    Maven Project Write

---
+++ src/test/java/org/cts/Rocks.java
@@ -0,0 +1,28 @@
+package org.cts;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Rocks {
+	public static void main(String[] args) throws Throwable {
+		File loc = new File("A:\\Isaacraja\\Space\\TestData\\raaa.xlsx");
+		Workbook w = new XSSFWorkbook();
+		Sheet s = w.createSheet("Isaac123");
+		Row r = s.createRow(1);
+		Cell c = r.createCell(3);
+		c.setCellValue("raja");
+		FileOutputStream o = new FileOutputStream(loc);
+		w.write(o);
+		System.out.println("Written Successfully");
+		
+	}
+	
+
+}
